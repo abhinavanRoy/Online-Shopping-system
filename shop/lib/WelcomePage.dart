f38899b0f1e30d57b09853b800782bdba3fc1b86
abhinavan-longview <abhinavan.longview@gmail.com>
2021-03-06 16:00:29 +0530

    updated ui

--- shop/lib/WelcomePage.dart
+++ shop/lib/WelcomePage.dart
@@ -15,74 +15,76 @@ class _WelcomePageState extends State<WelcomePage> {
         width: MediaQuery.of(context).size.width / 0.2,
         height: MediaQuery.of(context).size.height / 0.6,
         color: Colors.yellow,
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 160.0, 0.0, 0.0),
-              child: Text(
-                "Quick Shop",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
+        child: Expanded(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 160.0, 0.0, 0.0),
+                child: Text(
+                  "Quick Shop",
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-              child: Text(
-                "The best Online Shopping experience",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                child: Text(
+                  "The best Online Shopping experience",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(70.0, 10.0, 0.0, 0.0),
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context, MaterialPageRoute(builder: (context) => Login()));
-                      },
-                      color: Colors.black54,
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontSize: 17.0,
-                          color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(70.0, 10.0, 0.0, 0.0),
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => Login()));
+                        },
+                        color: Colors.black54,
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(55.0, 10.0, 0.0, 0.0),
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context, MaterialPageRoute(builder: (context) => Register()));
-                      },
-                      color: Colors.black54,
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                          fontSize: 17.0,
-                          color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(55.0, 10.0, 0.0, 0.0),
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Register()));
+                        },
+                        color: Colors.black54,
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
